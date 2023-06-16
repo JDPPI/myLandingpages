98773c2658211d20e96cd273863389dfc0c8d226
JDPPI <133482838+JDPPI@users.noreply.github.com>
2023-06-17 02:48:59 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+var typed = new Typed(".text", {
+    strings: ["Student" , "Future Game Developer" , "Future Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDeley:1000,
+    loop:true,
+    smartBackspace: true,
+    shuffle: false,
+    backDelay: 700,
+    fadeOut: false,
+  fadeOutClass: 'typed-fade-out',
+  fadeOutDelay: 500,
+  loop: false,
+  loopCount: Infinity,
+  showCursor: true,
+  cursorChar: '|',
+  autoInsertCss: true,
+  attr: null,
+  bindInputFocusEvents: false,
+  contentType: 'html',
+  onBegin: (self) => {},
+  onComplete: (self) => {},
+  preStringTyped: function (arrayPos, self) { },
+  onStringTyped: (arrayPos, self) => {},
+  onLastStringBackspaced: (self) => {},
+  onTypingPaused: (arrayPos, self) => {},
+  onTypingResumed: (arrayPos, self) => {},
+  onReset: (self) => {},
+  onStop: (arrayPos, self) => {},
+  onStart: (arrayPos, self) => {},
+  onDestroy: (self) => {},
+
+
+
+
+
+
+});
